3d91aa2da63af3858f481ada6ab1b48fdd48bcc2
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2020-06-05 21:59:33 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This react App is create by Muhammad Waleed Khan
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
